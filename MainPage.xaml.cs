1772c3ea62493a3f10eb71fa0df3acbde491c89c
OhRidgeInAll <8bitOriginal@gmail.com>
2025-02-04 10:06:31 -0500

    Initial implementation of logic

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace MathOperators;
+﻿using System.Diagnostics;
+using Microsoft.Maui.Graphics.Text;
+
+namespace MathOperators;
 
 public partial class MainPage : ContentPage
 {
@@ -7,4 +10,43 @@ public partial class MainPage : ContentPage
         InitializeComponent();
     }
 
+    //Take the values provided in the input fields and perform the selected operation on them
+    private void OnCalculateFields(object sender, EventArgs e)
+    {
+        //Get User Input for operation
+        double firstOperator = double.Parse(_txtLeftOp.Text);
+        double secondOperator = double.Parse(_txtRightOp.Text);
+        char opEntry = ((string)_pckOperand.SelectedItem)[0]; //cast to string is possible because SelectedItem is a String
+
+        //Perform the operation and obtain result --> Method
+        double result = PerformArithmeticOperation(opEntry, firstOperator, secondOperator);
+        //Output result to field. Show the work
+        _txtMathExp.Text = $"{firstOperator} {opEntry} {secondOperator} = {result}";
+    }
+
+    //Perform the Arithmetic Operation and obtain the result
+    private double PerformArithmeticOperation(char opEntry, double firstOperator, double secondOperator)
+    {
+        switch (opEntry)
+        {
+            case '+':
+                return firstOperator + secondOperator;
+            
+            case '-':
+                return firstOperator - secondOperator;
+            
+            case '*':
+                return firstOperator * secondOperator;
+            
+            case '/':
+                return firstOperator / secondOperator;
+            
+            case '%':
+                return firstOperator % secondOperator;
+            
+            default:
+                Debug.Assert(false ,"unknown arithmetic operation. Unable to perform arithmetic operation.");
+                return 0;
+        }
+    }
 }
