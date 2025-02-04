72d8844ad6b565e4d6ee850fdc7f1c79442d9b43
OhRidgeInAll <8bitOriginal@gmail.com>
2025-02-04 10:43:09 -0500

    Seperated Int Division and Real Division

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -14,14 +14,14 @@ public partial class MainPage : ContentPage
     private void OnCalculateFields(object sender, EventArgs e)
     {
         //Get User Input for operation
-        double firstOperator = double.Parse(_txtLeftOp.Text);
-        double secondOperator = double.Parse(_txtRightOp.Text);
+        double firstOperand = double.Parse(_txtLeftOp.Text);
+        double secondOperand = double.Parse(_txtRightOp.Text);
         char opEntry = ((string)_pckOperand.SelectedItem)[0]; //cast to string is possible because SelectedItem is a String
 
         //Perform the operation and obtain result --> Method
-        double result = PerformArithmeticOperation(opEntry, firstOperator, secondOperator);
+        double result = PerformArithmeticOperation(opEntry, firstOperand, secondOperand);
         //Output result to field. Show the work
-        _txtMathExp.Text = $"{firstOperator} {opEntry} {secondOperator} = {result}";
+        _txtMathExp.Text = $"{firstOperand} {opEntry} {secondOperand} = {result}";
     }
 
     //Perform the Arithmetic Operation and obtain the result
@@ -39,7 +39,7 @@ public partial class MainPage : ContentPage
                 return firstOperator * secondOperator;
             
             case '/':
-                return firstOperator / secondOperator;
+                return PerformDivision(firstOperator, secondOperator);
             
             case '%':
                 return firstOperator % secondOperator;
@@ -47,6 +47,22 @@ public partial class MainPage : ContentPage
             default:
                 Debug.Assert(false ,"unknown arithmetic operation. Unable to perform arithmetic operation.");
                 return 0;
+        }
+    }
+
+    private double PerformDivision(double firstOperator, double secondOperator)
+    {
+        string divOp = _pckOperand.SelectedItem as string;
+        if (divOp.Contains("Int", StringComparison.OrdinalIgnoreCase))
+        {
+            //Integer division
+            int result = (int)firstOperator / (int)secondOperator;
+            return result;
+        }
+        else
+        {
+            //Real division
+            return firstOperator / secondOperator;
         }
     }
 }
