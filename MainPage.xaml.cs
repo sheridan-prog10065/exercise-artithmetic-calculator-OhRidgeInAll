0421d2687419d3b223ab4c091d0cf9b44d5b828d
OhRidgeInAll <8bitOriginal@gmail.com>
2025-03-25 08:20:12 -0400

    Initial start to error handling

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,28 +1,59 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
 using Microsoft.Maui.Graphics.Text;
 
 namespace MathOperators;
 
 public partial class MainPage : ContentPage
 {
+    /// <summary>
+    /// Remembers the expressions calculated by user
+    /// </summary>
+    private ObservableCollection<string> _expList;
     public MainPage()
     {
         InitializeComponent();
+        
+        _expList = new ObservableCollection<string>();
+        _lstExpHistory.ItemsSource = _expList;
+
     }
 
     //Take the values provided in the input fields and perform the selected operation on them
-    private void OnCalculateFields(object sender, EventArgs e)
+    private async void OnCalculateFields(object sender, EventArgs e)
     {
-        //Get User Input for operation
-        double firstOperand = double.Parse(_txtLeftOp.Text);
-        double secondOperand = double.Parse(_txtRightOp.Text);
-        char opEntry = ((string)_pckOperand.SelectedItem)[0]; //cast to string is possible because SelectedItem is a String
+        try
+        {
+            //Get User Input for operation
+            var firstOperand = ReadFirstOperand();
+            double secondOperand = double.Parse(_txtRightOp.Text);
+            char opEntry = ((string)_pckOperand.SelectedItem)[0]; //cast to string is possible because SelectedItem is a String
 
-        //Perform the operation and obtain result --> Method
-        double result = PerformArithmeticOperation(opEntry, firstOperand, secondOperand);
-        //Output result to field. Show the work
-        _txtMathExp.Text = $"{firstOperand} {opEntry} {secondOperand} = {result}";
+            //Perform the operation and obtain result --> Method
+            double result = PerformArithmeticOperation(opEntry, firstOperand, secondOperand);
+            //Output result to field. Show the work
+            string expression = $"{firstOperand} {opEntry} {secondOperand} = {result}";
+        
+            //Remember the expression
+            _expList.Add(expression);
+
+            _txtMathExp.Text = null;
+            _txtMathExp.Text = $"{firstOperand} {opEntry} {secondOperand} = {result}";
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception);
+            throw;
+        }
+        
     }
+
+    private double ReadFirstOperand()
+    {
+        double firstOperand = double.Parse(_txtLeftOp.Text);
+        return firstOperand;
+    }
+
 
     //Perform the Arithmetic Operation and obtain the result
     private double PerformArithmeticOperation(char opEntry, double firstOperator, double secondOperator)
@@ -48,6 +79,7 @@ public partial class MainPage : ContentPage
                 Debug.Assert(false ,"unknown arithmetic operation. Unable to perform arithmetic operation.");
                 return 0;
         }
+        
     }
 
     private double PerformDivision(double firstOperator, double secondOperator)
